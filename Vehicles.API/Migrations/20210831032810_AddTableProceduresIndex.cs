e2b18664bc4e75a4d6ca64a8e52b661f5f41e1c8
JHON WILLIAM TEJADA DURANGO <jhontejada112531@correo.itm.edu.co>
2021-08-30 23:32:03 -0500

    Entity creation progress

---
+++ Vehicles.API/Migrations/20210831032810_AddTableProceduresIndex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicles.API.Migrations
+{
+    public partial class AddTableProceduresIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Procedimientos_Description",
+                table: "Procedimientos",
+                column: "Description",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Procedimientos_Description",
+                table: "Procedimientos");
+        }
+    }
+}
---
+++ Vehicles.API/Migrations/20210831034140_UpdateTableProcedures.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicles.API.Migrations
+{
+    public partial class UpdateTableProcedures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Procedimientos",
+                table: "Procedimientos");
+
+            migrationBuilder.RenameTable(
+                name: "Procedimientos",
+                newName: "Procedures");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Procedimientos_Description",
+                table: "Procedures",
+                newName: "IX_Procedures_Description");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Procedures",
+                table: "Procedures",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Procedures",
+                table: "Procedures");
+
+            migrationBuilder.RenameTable(
+                name: "Procedures",
+                newName: "Procedimientos");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Procedures_Description",
+                table: "Procedimientos",
+                newName: "IX_Procedimientos_Description");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Procedimientos",
+                table: "Procedimientos",
+                column: "Id");
+        }
+    }
+}
