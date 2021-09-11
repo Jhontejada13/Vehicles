d79879b6efc9bc531122b47570c64aedc6f1fe46
JHON WILLIAM TEJADA DURANGO <jhontejada112531@correo.itm.edu.co>
2021-09-11 14:30:12 -0500

    Add login and logout

---
+++ Vehicles.API/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.API.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un email válido.")]
+        [Required(ErrorMessage = "El Campo {0} es obligatorio")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        [MinLength(6, ErrorMessage = "El campo {0} no puede tener un longitud mínima de {1} caracteres.")]
+        [Required(ErrorMessage = "El Campo {0} es obligatorio")]
+        public string Password { get; set; }
+
+        [Display(Name = "Recordarme")]
+        public bool RememberMe { get; set; }
+    }
+}
