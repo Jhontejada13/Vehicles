fd765679f5f1f3dbf0628e77b7a6fecaa834e5df
JHON WILLIAM TEJADA DURANGO <jhontejada112531@correo.itm.edu.co>
2021-09-25 13:00:42 -0500

    Change user code

--- Vehicles.API/Controllers/AccountController.cs
+++ Vehicles.API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -136,5 +137,172 @@ namespace Vehicles.API.Controllers
             model.DocumentTypes = _combosHelper.GetCombosDocumentsType();
             return View(model);
         }
+
+        public async Task<IActionResult> ChangeUser()
+        {
+            User user = await _userHelper.GetUserAsync(User.Identity.Name);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            EditUserViewModel model = new()
+            {
+                Address = user.Address,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                PhoneNumber = user.PhoneNumber,
+                ImageId = user.ImageId,
+                Id = user.Id,
+                Document = user.Document,
+                DocumentTypeId = user.DocumentType.Id,
+                DocumentTypes = _combosHelper.GetCombosDocumentsType(),
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangeUser(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = model.ImageId;
+
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+
+                User user = await _userHelper.GetUserAsync(User.Identity.Name);
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Address = model.Address;
+                user.PhoneNumber = model.PhoneNumber;
+                user.ImageId = imageId;
+                user.DocumentType = await _context.DocumentTypes.FindAsync(model.DocumentTypeId);
+                user.Document = model.Document;
+                await _userHelper.UpdateUserAsync(user);
+                return RedirectToAction("Index", "Home");
+            }
+
+            model.DocumentTypes = _combosHelper.GetCombosDocumentsType();
+            return View(model);
+        }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userHelper.GetUserAsync(User.Identity.Name);
+                if (user != null)
+                {
+                    IdentityResult result = await _userHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction(nameof(ChangeUser));
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, result.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Usuario no encontrado.");
+                }
+            }
+
+            return View(model);
+        }
+
+        //public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        //{
+        //    if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(token))
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    User user = await _userHelper.GetUserAsync(new Guid(userId));
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    IdentityResult result = await _userHelper.ConfirmEmailAsync(user, token);
+        //    if (!result.Succeeded)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View();
+        //}
+
+        //public IActionResult RecoverPassword()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> RecoverPassword(RecoverPasswordViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        User user = await _userHelper.GetUserAsync(model.Email);
+        //        if (user == null)
+        //        {
+        //            ModelState.AddModelError(string.Empty, "El correo ingresado no corresponde a ningún usuario.");
+        //            return View(model);
+        //        }
+
+        //        string myToken = await _userHelper.GeneratePasswordResetTokenAsync(user);
+        //        string link = Url.Action(
+        //            "ResetPassword",
+        //            "Account",
+        //            new { token = myToken }, protocol: HttpContext.Request.Scheme);
+        //        _mailHelper.SendMail(model.Email, "Vehicles - Reseteo de contraseña", $"<h1>Vehicles - Reseteo de contraseña</h1>" +
+        //            $"Para establecer una nueva contraseña haga clic en el siguiente enlace:</br></br>" +
+        //            $"<a href = \"{link}\">Cambio de Contraseña</a>");
+        //        ViewBag.Message = "Las instrucciones para el cambio de contraseña han sido enviadas a su email.";
+        //        return View();
+
+        //    }
+
+        //    return View(model);
+        //}
+
+        //public IActionResult ResetPassword(string token)
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        //{
+        //    User user = await _userHelper.GetUserAsync(model.UserName);
+        //    if (user != null)
+        //    {
+        //        IdentityResult result = await _userHelper.ResetPasswordAsync(user, model.Token, model.Password);
+        //        if (result.Succeeded)
+        //        {
+        //            ViewBag.Message = "Contaseña cambiada.";
+        //            return View();
+        //        }
+
+        //        ViewBag.Message = "Error cambiando la contraseña.";
+        //        return View(model);
+        //    }
+
+        //    ViewBag.Message = "Usuario no encontrado.";
+        //    return View(model);
+        //}
+
     }
 }
