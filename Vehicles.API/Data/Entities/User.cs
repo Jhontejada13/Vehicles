65d50f37d8b84a688d793c2a44010788b5754e93
JHON WILLIAM TEJADA DURANGO <jhontejada112531@correo.itm.edu.co>
2021-09-25 17:28:32 -0500

    Publish

--- Vehicles.API/Data/Entities/User.cs
+++ Vehicles.API/Data/Entities/User.cs
@@ -36,7 +36,7 @@ namespace Vehicles.API.Data.Entities
 
         [Display(Name = "Foto")]
         public string ImageFullPath => ImageId == Guid.Empty
-            ? $"https://localhost:44395/images/no-image.png"
+            ? $"https://vehiclesapijhontd.azurewebsites.net/images/no-image.png"
             : $"https://vehiclesjhon.blob.core.windows.net/users/{ImageId}";
 
         [Display(Name = "Tipo de usuario")]
