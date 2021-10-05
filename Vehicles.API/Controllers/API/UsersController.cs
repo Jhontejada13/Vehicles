2c478f523e1b9f3e9c9f046307d56abce30cf7f5
JHON WILLIAM TEJADA DURANGO <jhontejada112531@correo.itm.edu.co>
2021-10-04 23:33:23 -0500

    Begin UsersController

---
+++ Vehicles.API/Controllers/API/UsersController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+using Vehicles.Common.Enums;
+
+namespace Vehicles.API.Controllers.API
+{
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)] // Se puede poner a nivel de método o a nivel de clase
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users
+                .Include(x => x.DocumentType)
+                .OrderBy(x => x.FirstName)
+                .ThenBy(x => x.LastName)
+                .Where(x => x.UserType == UserType.User)
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(string id)
+        {
+            User user = await _context.Users
+                .Include(x => x.DocumentType)
+                .Include(x => x.Vehicles)
+                .ThenInclude(x => x.VehicleType)
+                .Include(x => x.Vehicles)
+                .ThenInclude(x => x.Brand)
+                .Include(x => x.Vehicles)
+                .ThenInclude(x => x.VehiclePhotos)
+                .Include(x => x.Vehicles)
+                .ThenInclude(x => x.Histories)
+                .ThenInclude(x => x.Details)
+                .ThenInclude(x => x.Procedure)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVehicleType(int id, VehicleType vehicleType)
+        {
+            if (id != vehicleType.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vehicleType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe este tipo de vehículo");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+    }
+}
